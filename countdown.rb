09777cd1721051d4c27d51d34e83435602ff2ddf
Edsey omenda <omendajames0317@gmail.com>
2022-08-18 16:18:44 +0300

    completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    
+while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
 end
+return "HAPPY NEW YEAR!"
+end
+
+countdown(10)
+
+
+def countdown_with_sleep(number, how_long=5)
+    
+while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep how_long
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+  end
+
+  countdown_with_sleep(5)
